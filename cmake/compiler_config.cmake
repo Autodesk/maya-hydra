7f159ab29deedaa316135da51b893b8e20fa263b
debloip <philippe.de.blois@autodesk.com>
2023-12-22 11:29:39 -0500

    HYDRA-700 : Avoid Boost autolinking libraries

--- cmake/compiler_config.cmake
+++ cmake/compiler_config.cmake
@@ -131,9 +131,10 @@ function(mayaHydra_compile_config TARGET)
         )
     endif()
 
-    # Remove annoying TBB warnings.
+    # Common definitions
     target_compile_definitions(${TARGET}
         PRIVATE
-            TBB_SUPPRESS_DEPRECATED_MESSAGES
+            TBB_SUPPRESS_DEPRECATED_MESSAGES # Remove TBB deprecation warnings
+            BOOST_ALL_NO_LIB # Avoid Boost autolinking libraries
     )
 endfunction()
