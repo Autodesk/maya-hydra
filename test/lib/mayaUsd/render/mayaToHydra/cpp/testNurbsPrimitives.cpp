97541c487aeb055d5ea4533ef5eac55e2054db3e
debloip <philippe.de.blois@autodesk.com>
2024-01-15 11:54:51 -0500

    HYDRA-780 : Add NURBS test files

---
+++ test/lib/mayaUsd/render/mayaToHydra/cpp/testNurbsPrimitives.cpp
@@ -0,0 +1,54 @@
+// Copyright 2024 Autodesk
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include "testUtils.h"
+
+#include <mayaHydraLib/hydraUtils.h>
+#include <mayaHydraLib/mayaUtils.h>
+
+#include <pxr/imaging/hd/tokens.h>
+
+#include <maya/MViewport2Renderer.h>
+
+#include <gtest/gtest.h>
+
+PXR_NAMESPACE_USING_DIRECTIVE
+
+using namespace MayaHydra;
+
+TEST(NurbsPrimitives, nurbsSphere)
+{
+    // Setup inspector for the first scene index
+    const SceneIndicesVector& sceneIndices = GetTerminalSceneIndices();
+    ASSERT_GT(sceneIndices.size(), 0u);
+    SceneIndexInspector inspector(sceneIndices.front());
+
+    // Retrieve the sphere prim
+    FindPrimPredicate findSpherePredicate
+        = [](const HdSceneIndexBasePtr& sceneIndex, const SdfPath& primPath) -> bool {
+        HdSceneIndexPrim prim = sceneIndex->GetPrim(primPath);
+        if (prim.primType != HdPrimTypeTokens->mesh) {
+            return false;
+        }
+        bool parentIsNurbsSphereShape
+            = MakeRelativeToParentPath(primPath.GetParentPath()).GetAsString().find("nurbsSphereShape") != std::string::npos;
+        return parentIsNurbsSphereShape;
+    };
+    PrimEntriesVector foundPrims = inspector.FindPrims(findSpherePredicate, 1);
+    ASSERT_EQ(foundPrims.size(), 1u);
+    HdSceneIndexPrim spherePrim = foundPrims.front().prim;
+    EXPECT_TRUE(spherePrim.primType != TfToken());
+    EXPECT_TRUE(spherePrim.dataSource != nullptr);
+}
