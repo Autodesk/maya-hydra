86d1d1a4dc8b684d0028c2aa1d19fbc617deab13
debloip <philippe.de.blois@autodesk.com>
2024-01-24 10:12:48 -0500

    HYDRA-815 : Remove platform import

--- test/lib/mayaUsd/render/mayaToHydra/testDirectionalLights.py
+++ test/lib/mayaUsd/render/mayaToHydra/testDirectionalLights.py
@@ -18,7 +18,6 @@ import maya.cmds as cmds
 import fixturesUtils
 import mtohUtils
 import mayaUtils
-import platform
 import unittest
 
 class TestDirectionalLights(mtohUtils.MtohTestCase): #Subclassing mtohUtils.MtohTestCase to be able to call self.assertSnapshotClose
