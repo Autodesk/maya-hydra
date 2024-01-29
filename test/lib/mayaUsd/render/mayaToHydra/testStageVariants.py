05ac5fce87ae3c248682d6a19c10aa6e6ae8a776
David Lanier <david.lanier@autodesk.com>
2024-01-29 10:41:17 +0100

    HYDRA-801 : Add a test for Usd stages variants

---
+++ test/lib/mayaUsd/render/mayaToHydra/testStageVariants.py
@@ -0,0 +1,119 @@
+#
+# Copyright 2024 Autodesk, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import maya.cmds as cmds
+import fixturesUtils
+import mtohUtils
+import unittest
+import testUtils
+import mayaUtils
+import usdUtils
+import ufe
+
+class TestStageVariants(mtohUtils.MtohTestCase): #Subclassing mtohUtils.MtohTestCase to be able to call self.assertSnapshotClose
+    # MayaHydraBaseTestCase.setUpClass requirement.
+    _file = __file__
+
+    IMAGE_DIFF_FAIL_THRESHOLD = 0.1
+    IMAGE_DIFF_FAIL_PERCENT = 0.3
+
+    @unittest.skipUnless(mtohUtils.checkForMayaUsdPlugin(), "Requires Maya USD Plugin.")
+    def test_UsdStageVariants(self):
+        from mayaUsd import lib as mayaUsdLib
+        cmds.file(new=True, force=True)
+
+        testFile = mayaUtils.openTestScene(
+                "testStageVariants",
+                "testStageVariants.ma")
+        self.setHdStormRenderer()
+        cmds.refresh()
+
+        shapeNode = "variantsShape"
+        shapeStage = mayaUsdLib.GetPrim(shapeNode).GetStage()
+        
+        #Check geometry variant
+        rootPrim = shapeStage.GetPrimAtPath('/Cubes')
+        self.assertFalse(rootPrim== None)
+        modVariant = rootPrim.GetVariantSet('modelingVariant')
+        self.assertFalse(modVariant== None)
+        self.assertEqual(modVariant.GetVariantSelection(), 'OneCube')
+
+        #Select the USD Cubes to see the selection highlight
+        cubesPath = ufe.Path([
+            mayaUtils.createUfePathSegment("|variants|variantsShape"), 
+            usdUtils.createUfePathSegment("/Cubes")])
+        cubesItems = ufe.Hierarchy.createItem(cubesPath)
+        self.assertIsNotNone(cubesItems)
+        ufe.GlobalSelection.get().clear()
+        ufe.GlobalSelection.get().append(cubesItems)
+        self.assertSnapshotClose("oneCube.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
+        ufe.GlobalSelection.get().clear()
+        
+        modVariant.SetVariantSelection('TwoCubes')
+        self.assertEqual(modVariant.GetVariantSelection(), 'TwoCubes')
+        ufe.GlobalSelection.get().append(cubesItems)
+        self.assertSnapshotClose("twoCubes.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
+        ufe.GlobalSelection.get().clear()
+        
+        modVariant.SetVariantSelection('ThreeCubes')
+        self.assertEqual(modVariant.GetVariantSelection(), 'ThreeCubes')
+        ufe.GlobalSelection.get().append(cubesItems)
+        self.assertSnapshotClose("threeCubes.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
+        ufe.GlobalSelection.get().clear()
+        
+        #Check displacement variant for CubeOne (not an actual displacement but a different transform)
+        cube1Prim = shapeStage.GetPrimAtPath('/Cubes/Geom/CubeOne')
+        self.assertFalse(cube1Prim== None)
+        displacementVariant = cube1Prim.GetVariantSet('displacement')
+        self.assertFalse(displacementVariant== None)
+        self.assertEqual(displacementVariant.GetVariantSelection(), 'none')
+        displacementVariant .SetVariantSelection('moved')
+        self.assertEqual(displacementVariant.GetVariantSelection(), 'moved')
+        ufe.GlobalSelection.get().append(cubesItems)
+        self.assertSnapshotClose("threeCubesWithDisplacement.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
+        ufe.GlobalSelection.get().clear()
+
+        #Check if it works when the prims are instanceable
+        cube2Prim = shapeStage.GetPrimAtPath('/Cubes/Geom/CubeTwo')
+        self.assertFalse(cube2Prim== None)
+        cube3Prim = shapeStage.GetPrimAtPath('/Cubes/Geom/CubeThree')
+        self.assertFalse(cube3Prim== None)
+        cube1Prim.SetInstanceable(True)
+        self.assertTrue(cube1Prim.IsInstanceable())
+        cube2Prim.SetInstanceable(True)
+        self.assertTrue(cube2Prim.IsInstanceable())
+        cube3Prim.SetInstanceable(True)
+        self.assertTrue(cube3Prim.IsInstanceable())
+        modVariant.SetVariantSelection('OneCube')
+        self.assertEqual(modVariant.GetVariantSelection(), 'OneCube')
+        ufe.GlobalSelection.get().append(cubesItems)
+        self.assertSnapshotClose("oneCubeInstanceable.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
+        ufe.GlobalSelection.get().clear()
+
+        #Enable the following tests when HYDRA-820 is fixed
+        #modVariant.SetVariantSelection('TwoCubes')
+        #self.assertEqual(modVariant.GetVariantSelection(), 'TwoCubes')
+        #ufe.GlobalSelection.get().append(cubesItems)
+        #self.assertSnapshotClose("twoCubesInstanceable.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
+        #ufe.GlobalSelection.get().clear()
+
+        #modVariant.SetVariantSelection('ThreeCubes')
+        #self.assertEqual(modVariant.GetVariantSelection(), 'ThreeCubes')
+        #ufe.GlobalSelection.get().append(cubesItems) 
+        #self.assertSnapshotClose("threeCubesInstanceable.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
+
+if __name__ == '__main__':
+    fixturesUtils.runTests(globals())
