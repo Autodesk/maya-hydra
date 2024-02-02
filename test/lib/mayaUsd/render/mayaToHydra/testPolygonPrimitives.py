ffba728e899ccad5b205c097f46dcfaa2714c6f6
debloip <philippe.de.blois@autodesk.com>
2024-02-02 11:23:23 -0500

    HYDRA-781 : Add tests for pyramid, prism, pipe, helix, gear, soccerball

--- test/lib/mayaUsd/render/mayaToHydra/testPolygonPrimitives.py
+++ test/lib/mayaUsd/render/mayaToHydra/testPolygonPrimitives.py
@@ -142,5 +142,122 @@ class TestPolygonPrimitives(mtohUtils.MtohTestCase):
         cmds.setAttr(polyCreatorNodeName + ".subdivisionMode", 1)
         self.compareSnapshot("platonic_icosahedron_triangles.png")
 
+    def test_PolygonPyramid(self):
+        polyCreatorNodeName = self.setupScene(cmds.polyPyramid)[1]
+        self.compareSnapshot("pyramid_fresh.png")
+
+        cmds.setAttr(polyCreatorNodeName + ".sideLength", 5)
+        cmds.setAttr(polyCreatorNodeName + ".heightBaseline", -0.5)
+        cmds.setAttr(polyCreatorNodeName + ".subdivisionsHeight", 5)
+        cmds.setAttr(polyCreatorNodeName + ".subdivisionsCaps", 2)
+        cmds.setAttr(polyCreatorNodeName + ".axisX", 1)
+        cmds.setAttr(polyCreatorNodeName + ".axisY", -3)
+        cmds.setAttr(polyCreatorNodeName + ".axisZ", -0.25)
+
+        cmds.setAttr(polyCreatorNodeName + ".numberOfSides", 3)
+        self.compareSnapshot("pyramid_3sides.png")
+
+        cmds.setAttr(polyCreatorNodeName + ".numberOfSides", 4)
+        self.compareSnapshot("pyramid_4sides.png")
+
+        cmds.setAttr(polyCreatorNodeName + ".numberOfSides", 5)
+        self.compareSnapshot("pyramid_5sides.png")
+
+    def test_PolygonPrism(self):
+        polyCreatorNodeName = self.setupScene(cmds.polyPrism)[1]
+        self.compareSnapshot("prism_fresh.png")
+
+        cmds.setAttr(polyCreatorNodeName + ".length", 4)
+        cmds.setAttr(polyCreatorNodeName + ".sideLength", 5)
+        cmds.setAttr(polyCreatorNodeName + ".heightBaseline", 0.5)
+        cmds.setAttr(polyCreatorNodeName + ".subdivisionsHeight", 5)
+        cmds.setAttr(polyCreatorNodeName + ".subdivisionsCaps", 2)
+        cmds.setAttr(polyCreatorNodeName + ".axisX", 0.25)
+        cmds.setAttr(polyCreatorNodeName + ".axisY", -3)
+        cmds.setAttr(polyCreatorNodeName + ".axisZ", -0.5)
+
+        cmds.setAttr(polyCreatorNodeName + ".numberOfSides", 5)
+        self.compareSnapshot("prism_5sides.png")
+
+        cmds.setAttr(polyCreatorNodeName + ".numberOfSides", 8)
+        self.compareSnapshot("prism_8sides.png")
+
+    def test_PolygonPipe(self):
+        polyCreatorNodeName = self.setupScene(cmds.polyPipe)[1]
+        self.compareSnapshot("pipe_fresh.png")
+        
+        cmds.setAttr(polyCreatorNodeName + ".radius", 3)
+        cmds.setAttr(polyCreatorNodeName + ".height", 5)
+        cmds.setAttr(polyCreatorNodeName + ".thickness", 1)
+        cmds.setAttr(polyCreatorNodeName + ".heightBaseline", 0.5)
+        cmds.setAttr(polyCreatorNodeName + ".subdivisionsAxis", 10)
+        cmds.setAttr(polyCreatorNodeName + ".subdivisionsHeight", 5)
+        cmds.setAttr(polyCreatorNodeName + ".subdivisionsCaps", 2)
+        cmds.setAttr(polyCreatorNodeName + ".axisX", 1)
+        cmds.setAttr(polyCreatorNodeName + ".axisY", 2)
+        cmds.setAttr(polyCreatorNodeName + ".axisZ", -0.25)
+        self.compareSnapshot("pipe_modified.png")
+
+        cmds.setAttr(polyCreatorNodeName + ".roundCap", True)
+        self.compareSnapshot("pipe_roundCap.png")
+
+        cmds.setAttr(polyCreatorNodeName + ".roundCapHeightCompensation", True)
+        self.compareSnapshot("pipe_roundCapHeightCompensation.png")
+
+    def test_PolygonHelix(self):
+        polyCreatorNodeName = self.setupScene(cmds.polyHelix)[1]
+        self.compareSnapshot("helix_fresh.png")
+        
+        cmds.setAttr(polyCreatorNodeName + ".coils", 2)
+        cmds.setAttr(polyCreatorNodeName + ".height", 5)
+        cmds.setAttr(polyCreatorNodeName + ".width", 5)
+        cmds.setAttr(polyCreatorNodeName + ".radius", 0.5)
+        cmds.setAttr(polyCreatorNodeName + ".heightBaseline", 0.5)
+        cmds.setAttr(polyCreatorNodeName + ".subdivisionsAxis", 10)
+        cmds.setAttr(polyCreatorNodeName + ".subdivisionsCoil", 10)
+        cmds.setAttr(polyCreatorNodeName + ".subdivisionsCaps", 1)
+        cmds.setAttr(polyCreatorNodeName + ".axisX", 1)
+        cmds.setAttr(polyCreatorNodeName + ".axisY", 2)
+        cmds.setAttr(polyCreatorNodeName + ".axisZ", -0.25)
+        self.compareSnapshot("helix_modified.png")
+
+        cmds.setAttr(polyCreatorNodeName + ".roundCap", True)
+        self.compareSnapshot("helix_roundCap.png")
+
+        cmds.setAttr(polyCreatorNodeName + ".direction", 0)
+        self.compareSnapshot("helix_clockwise.png")
+
+    def test_PolygonGear(self):
+        polyCreatorNodeName = self.setupScene(cmds.polyGear)[1]
+        self.compareSnapshot("gear_fresh.png")
+        
+        cmds.setAttr(polyCreatorNodeName + ".sides", 10)
+        cmds.setAttr(polyCreatorNodeName + ".radius", 5)
+        cmds.setAttr(polyCreatorNodeName + ".internalRadius", 2)
+        cmds.setAttr(polyCreatorNodeName + ".height", 5)
+        cmds.setAttr(polyCreatorNodeName + ".heightDivisions", 5)
+        cmds.setAttr(polyCreatorNodeName + ".heightBaseline", 0.5)
+        cmds.setAttr(polyCreatorNodeName + ".gearSpacing", 0.5)
+        cmds.setAttr(polyCreatorNodeName + ".gearOffset", 2)
+        cmds.setAttr(polyCreatorNodeName + ".gearTip", 1)
+        cmds.setAttr(polyCreatorNodeName + ".gearMiddle", 0.75)
+        cmds.setAttr(polyCreatorNodeName + ".twist", 10)
+        cmds.setAttr(polyCreatorNodeName + ".taper", 1.25)
+        self.compareSnapshot("gear_modified.png")
+
+    def test_PolygonSoccerBall(self):
+        polyCreatorNodeName = self.setupScene(cmds.polyPrimitive)[1]
+        self.compareSnapshot("soccerball_fresh.png")
+        
+        cmds.setAttr(polyCreatorNodeName + ".radius", 4)
+        cmds.setAttr(polyCreatorNodeName + ".sideLength", 2)
+        # Radius and side length are tied together, changing one changes the other
+        self.assertAlmostEqual(cmds.getAttr(polyCreatorNodeName + ".radius"), 4.955, 3)
+        cmds.setAttr(polyCreatorNodeName + ".heightBaseline", 0.5)
+        cmds.setAttr(polyCreatorNodeName + ".axisX", 1)
+        cmds.setAttr(polyCreatorNodeName + ".axisY", 2)
+        cmds.setAttr(polyCreatorNodeName + ".axisZ", -1)
+        self.compareSnapshot("soccerball_modified.png")
+
 if __name__ == '__main__':
     fixturesUtils.runTests(globals())
