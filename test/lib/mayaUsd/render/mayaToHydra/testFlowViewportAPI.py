40d3ff7f211ff0a89cc3bd8a753f31c5a6ba9329
debloip <philippe.de.blois@autodesk.com>
2024-04-16 13:59:49 -0400

    HYDRA-712 : Add test

--- test/lib/mayaUsd/render/mayaToHydra/testFlowViewportAPI.py
+++ test/lib/mayaUsd/render/mayaToHydra/testFlowViewportAPI.py
@@ -394,5 +394,63 @@ class TestFlowViewportAPI(mtohUtils.MayaHydraBaseTestCase): #Subclassing mtohUti
             self.setHdStormRenderer()
             self.assertSnapshotClose("multipleViewports_VP2AndThenBackToStorm_modPan2.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
 
+    def test_HierarchicalProperties(self):
+        with PluginLoaded('mayaHydraFlowViewportAPILocator'):
+            self.setBasicCam(10)
+
+            # Sphere that should be filtered by the locator filtering scene index (>10k vertices)
+            sphereShape, sphereNode = cmds.polySphere()
+            cmds.move(0, 0, 10)
+            cmds.setAttr(sphereNode + ".subdivisionsAxis", 200)
+            cmds.setAttr(sphereNode + ".subdivisionsHeight", 200)
+            cmds.select(clear=True)
+
+            locatorGrandParent = cmds.group(empty=True)
+            locatorParent = cmds.group(empty=True, parent=locatorGrandParent)
+            # Ensure the initial visibility is set correctly if a parent's visibility affects it
+            cmds.setAttr(locatorGrandParent + ".visibility", False)
+            locator = cmds.createNode("MhFlowViewportAPILocator", parent=locatorParent)
+            self.assertSnapshotClose("hierarchicalProperties_locator_visibility_off.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
+            # Check that updating a parent's visibility works after creation
+            cmds.setAttr(locatorGrandParent + ".visibility", True)
+            self.assertSnapshotClose("hierarchicalProperties_locator_visibility_on.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
+            # Check that changing the visibility on the shape itself works
+            cmds.setAttr(locator + ".visibility", False)
+            self.assertSnapshotClose("hierarchicalProperties_locator_visibility_off.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
+            
+            cmds.delete(sphereShape)
+            cmds.delete(locator)
+
+            # Set up a USD stage a with a cube
+            import mayaUsd_createStageWithNewLayer
+            import mayaUsd.lib
+            from pxr import UsdGeom
+            stagePath = mayaUsd_createStageWithNewLayer.createStageWithNewLayer()
+            stage = mayaUsd.lib.GetPrim(stagePath).GetStage()
+            UsdGeom.Cube.Define(stage, "/Cube")
+            cmds.select(clear=True)
+            stageParent = cmds.group(empty=True)
+            cmds.parent(stagePath, stageParent)
+            stageShape = stagePath.split('|')[-1]
+
+            # Hide/unhide parent
+            self.assertSnapshotClose("hierarchicalProperties_usdStage_visibility_on.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
+            cmds.setAttr(stageParent + ".visibility", False)
+            self.assertSnapshotClose("hierarchicalProperties_usdStage_visibility_off.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
+            cmds.setAttr(stageParent + ".visibility", True)
+            self.assertSnapshotClose("hierarchicalProperties_usdStage_visibility_on.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
+            # Hide/unhide the shape directly
+            cmds.setAttr(stageShape + ".visibility", False)
+            self.assertSnapshotClose("hierarchicalProperties_usdStage_visibility_off.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
+            cmds.setAttr(stageShape + ".visibility", True)
+            self.assertSnapshotClose("hierarchicalProperties_usdStage_visibility_on.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
+
+            # Change a parent transform
+            cmds.xform(stageParent, translation=[1,-2,3], rotation=[5,-10,15], scale=[1,-2,3])
+            self.assertSnapshotClose("hierarchicalProperties_usdStage_parentTransformChanged.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
+            # Change the shape's transform directly
+            cmds.xform(cmds.listRelatives(stageShape, parent=True)[0], translation=[-3,2,-1], rotation=[-15,10,-5], scale=[-2.5, 2.0, -1.5])
+            self.assertSnapshotClose("hierarchicalProperties_usdStage_shapeTransformChanged.png", self.IMAGE_DIFF_FAIL_THRESHOLD, self.IMAGE_DIFF_FAIL_PERCENT)
+
 if __name__ == '__main__':
     fixturesUtils.runTests(globals())
