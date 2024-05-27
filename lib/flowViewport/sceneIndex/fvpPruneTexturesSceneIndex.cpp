a3d429b2b5f65bd42f0a970fbf70dcd6a39ab415
Rakesh R <rakesh.roopavataram@autodesk.com>
2024-05-27 10:45:46 -0400

    Add Scene Index to support texture togglging

---
+++ lib/flowViewport/sceneIndex/fvpPruneTexturesSceneIndex.cpp
@@ -0,0 +1,103 @@
+// Copyright 2024 Autodesk
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include "flowViewport/sceneIndex/fvpPruneTexturesSceneIndex.h"
+#include "pxr/base/tf/staticTokens.h"
+#include "pxr/imaging/hd/sceneIndexPrimView.h"
+#include "pxr/imaging/hd/materialSchema.h"
+
+#include <iostream>
+namespace FVP_NS_DEF {
+
+PXR_NAMESPACE_USING_DIRECTIVE
+
+TF_DEFINE_PRIVATE_TOKENS(
+    _tokens,
+    (UsdPreviewSurface)
+    (ND_standard_surface_surfaceshader)
+);
+
+namespace {
+
+void
+_PruneTexturesFromMatNetwork(
+    HdMaterialNetworkInterface *networkInterface)
+{
+    if (!networkInterface) {
+        return;
+    }
+    const TfTokenVector nodeNames = networkInterface->GetNodeNames();
+    for (TfToken const &nodeName : nodeNames) {
+        const TfToken nodeType = networkInterface->GetNodeType(nodeName);
+        if (nodeType == _tokens->ND_standard_surface_surfaceshader ||
+            nodeType == _tokens->UsdPreviewSurface) {                
+            // Look for incoming connection(textures) to surface shader params
+            TfTokenVector inputConnections = networkInterface->GetNodeInputConnectionNames(nodeName);
+            for (TfToken const &connection : inputConnections) {
+                // Trivially  input connections.
+                networkInterface->DeleteNodeInputConnection(nodeName, connection);
+            }
+        }
+    }
+}
+
+} // Anonymous namespace
+
+// static
+PruneTexturesSceneIndexRefPtr
+PruneTexturesSceneIndex::New(
+    const HdSceneIndexBaseRefPtr &inputSceneIndex)
+{    
+    return TfCreateRefPtr(
+        new PruneTexturesSceneIndex(inputSceneIndex));
+}
+
+void
+PruneTexturesSceneIndex::MarkTexturesDirty(bool isTextured)
+{
+    _needsTexturesPruned = isTextured;
+    const HdDataSourceLocatorSet locators(
+    HdMaterialSchema::GetDefaultLocator()
+        .Append(HdMaterialSchemaTokens->material));
+
+    _DirtyAllPrims(locators);
+}
+
+PruneTexturesSceneIndex::PruneTexturesSceneIndex(
+    HdSceneIndexBaseRefPtr const &inputSceneIndex)
+  : HdMaterialFilteringSceneIndexBase(inputSceneIndex)
+{   
+}
+
+PruneTexturesSceneIndex::FilteringFnc 
+PruneTexturesSceneIndex::_GetFilteringFunction() const
+{
+    return !_needsTexturesPruned ? 
+            _PruneTexturesFromMatNetwork :
+            [](HdMaterialNetworkInterface*){};
+}
+
+void
+PruneTexturesSceneIndex::_DirtyAllPrims(
+    const HdDataSourceLocatorSet locators)
+{
+    HdSceneIndexObserver::DirtiedPrimEntries entries;
+    for (const SdfPath &path : HdSceneIndexPrimView(_GetInputSceneIndex())) {
+        entries.push_back({path, locators});
+    }
+    _SendPrimsDirtied(entries);
+}
+
+} //end of namespace FVP_NS_DEF
---
+++ lib/flowViewport/sceneIndex/fvpPruneTexturesSceneIndex.h
@@ -0,0 +1,56 @@
+// Copyright 2024 Autodesk
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#ifndef FVP_PRUNE_TEXTURE_SCENE_INDEX_NEW_H
+#define FVP_PRUNE_TEXTURE_SCENE_INDEX_NEW_H
+
+#include "flowViewport/api.h"
+#include "flowViewport/sceneIndex/fvpSceneIndexUtils.h"
+
+#include "pxr/imaging/hdsi/api.h"
+#include "pxr/imaging/hd/materialFilteringSceneIndexBase.h"
+#include "pxr/imaging/hd/materialNetworkInterface.h"
+
+namespace FVP_NS_DEF {
+
+class PruneTexturesSceneIndex;
+typedef PXR_NS::TfRefPtr<PruneTexturesSceneIndex> PruneTexturesSceneIndexRefPtr;
+typedef PXR_NS::TfRefPtr<const PruneTexturesSceneIndex> PruneTexturesSceneIndexConstRefPtr;
+
+class PruneTexturesSceneIndex :
+    public PXR_NS::HdMaterialFilteringSceneIndexBase
+{
+public:
+    FVP_API
+    static PruneTexturesSceneIndexRefPtr New(
+            const PXR_NS::HdSceneIndexBaseRefPtr &inputScene);
+
+    FVP_API
+    void MarkTexturesDirty(bool isTextured);
+    
+    bool _needsTexturesPruned = false;
+    
+    void _DirtyAllPrims(const PXR_NS::HdDataSourceLocatorSet locators);
+    
+protected:
+    FilteringFnc _GetFilteringFunction() const override;
+    
+private:
+    PruneTexturesSceneIndex(
+        PXR_NS::HdSceneIndexBaseRefPtr const &inputSceneIndex);
+};
+
+} //end of namespace FVP_NS_DEF
+
+#endif //FVP_PRUNE_TEXTURE_SCENE_INDEX_H
