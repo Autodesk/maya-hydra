afaef8329460d334e27386546844a8a9470daf90
debloip <philippe.de.blois@autodesk.com>
2025-01-16 17:23:20 -0500

    HYDRA-1271 : Bugfix for point instancer highlight

--- lib/flowViewport/sceneIndex/wireframeHighlights/piInstancerWhSi.cpp
+++ lib/flowViewport/sceneIndex/wireframeHighlights/piInstancerWhSi.cpp
@@ -135,7 +135,7 @@ HdSceneIndexPrim PiInstancerWhSi::GetHighlightPrim(const SdfPath &selectionPath,
     if (prim.primType == HdPrimTypeTokens->mesh) {
         prim.dataSource = SetWireframeRepr(prim.dataSource, primSelection.nestedInstanceIndices.empty() ? _wireframeColorInterface->getWireframeColor(selectionKey.first) : _wireframeColorInterface->getWireframeColor(primSelection));
     }
-    if (_IsPointInstancer(prim) && originalPath == selectionKey.first) {
+    if (_IsPointInstancer(prim) && originalPath == selectionKey.first && selectionKey.second != kFullHighlight) {
         // Adjust the instancer mask to only show selected instances
         HdSelectionsSchema selectionsSchema = HdSelectionsSchema::GetFromParent(prim.dataSource);
         HdSelectionSchema activeSelection = selectionsSchema.GetElement(std::stoul(selectionKey.second));
