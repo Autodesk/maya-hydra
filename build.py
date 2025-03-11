ddb55ed7401bea9939065edddd9fc681c8c8da6a
debloip <philippe.de.blois@autodesk.com>
2025-03-11 13:59:42 -0400

    HYDRA-1439 : Basic build setup

--- build.py
+++ build.py
@@ -507,6 +507,10 @@ def BuildAndInstall(context, buildArgs, stages):
             extraArgs.append('-DMAYA_LOCATION="{mayaLocation}"'
                              .format(mayaLocation=context.mayaLocation))
         
+        if context.viewportToolboxLocation:
+            extraArgs.append('-DVIEWPORTTOOLBOX_LOCATION="{viewportToolboxLocation}"'
+                             .format(viewportToolboxLocation=context.viewportToolboxLocation))
+        
         if context.mayaUsdLocation:
             extraArgs.append('-DMAYAUSD_LOCATION="{mayaUsdLocation}"'
                              .format(mayaUsdLocation=context.mayaUsdLocation))
@@ -585,6 +589,9 @@ parser.add_argument("--install-location", type=str,
 
 parser.add_argument("--maya-location", type=str,
                     help="Directory where Maya is installed.")
+
+parser.add_argument("--viewporttoolbox-location", type=str,
+                    help="Directory where ViewportToolbox is installed.")
 
 parser.add_argument("--mayausd-location", type=str,
                     help="Directory where MayaUsd is installed.")
@@ -717,6 +724,10 @@ class InstallContext:
         # Maya Location
         self.mayaLocation = (os.path.abspath(args.maya_location)
                                 if args.maya_location else None)
+        
+        # ViewportToolbox Location
+        self.viewportToolboxLocation = (os.path.abspath(args.viewporttoolbox_location).replace("\\","/")
+                                if args.viewporttoolbox_location else None)
 
         # MayaUsd Location
         self.mayaUsdLocation = (os.path.abspath(args.mayausd_location).replace("\\","/")
