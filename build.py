640e89014d27ac715aaec39f2021226dcf2b8302
ppt-adsk <pierre.tremblay@autodesk.com>
2025-04-02 14:30:15 -0400

    Fix --mtoa-location build.py flag. (#263)

--- build.py
+++ build.py
@@ -512,7 +512,7 @@ def BuildAndInstall(context, buildArgs, stages):
                              .format(mayaUsdLocation=context.mayaUsdLocation))
         
         if context.mtoaLocation:
-            extraArgs.append('-DMTOAUSD_LOCATION="{mtoaLocation}"' 
+            extraArgs.append('-DMTOA_LOCATION="{mtoaLocation}"' 
                              .format(mtoaLocation=context.mtoaLocation))
         
         if context.lookdevxLocation:
