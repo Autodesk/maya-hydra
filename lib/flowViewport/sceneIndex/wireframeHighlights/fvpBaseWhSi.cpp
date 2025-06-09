89e51449f35e1a82a20b4a987676fb4de02ad0d3
Rémi Cossette-Roberge <remi.cossette-roberge@autodesk.com>
2025-06-09 11:40:58 -0400

    usd proper version

--- lib/flowViewport/sceneIndex/wireframeHighlights/fvpBaseWhSi.cpp
+++ lib/flowViewport/sceneIndex/wireframeHighlights/fvpBaseWhSi.cpp
@@ -814,7 +814,7 @@ BaseWhSi::MakeGeomSubsetHighlight(
     if (!GetMaterialPath(geomSubsetPrimDataSource).IsEmpty()) {
         HdContainerDataSourceEditor dataSourceEditor(editedMeshPrimDataSource);
         dataSourceEditor.Set(HdMaterialBindingsSchema::GetDefaultLocator(), HdContainerDataSource::Get(geomSubsetPrimDataSource, HdMaterialBindingsSchema::GetDefaultLocator()));
-#if PXR_VERSION >= 2511
+#if PXR_VERSION >= 2505
         dataSourceEditor.Set(UsdImagingMaterialBindingsSchema::GetDefaultLocator(), HdContainerDataSource::Get(geomSubsetPrimDataSource, UsdImagingMaterialBindingsSchema::GetDefaultLocator()));
 #elif PXR_VERSION >= 2403
         dataSourceEditor.Set(UsdImagingDirectMaterialBindingsSchema::GetDefaultLocator(), HdContainerDataSource::Get(geomSubsetPrimDataSource, UsdImagingDirectMaterialBindingsSchema::GetDefaultLocator()));
