24a3377ac8179a656d650b97941926dacf3f1560
ADS\lanierd <david.lanier@autodesk.com>
2025-09-24 11:50:58 +0200

    Fix one test with usd 25.08 and two_ passes

--- test/lib/mayaUsd/render/mayaToHydra/testRefinement.py
+++ test/lib/mayaUsd/render/mayaToHydra/testRefinement.py
@@ -37,13 +37,15 @@ class TestRefinement(mtohUtils.MayaHydraBaseTestCase):
         #modify light intensity for usd 24.11+
         self.modifyDefaultLightIntensityByUsdVersion()
 
-    def verifySnapshot(self, imageName, useDynamicVersion=False):
+    def verifySnapshot(self, imageName, useDynamicVersion=False, usdImageVersion=None):
         cmds.refresh()
         imageVersion = None
         if useDynamicVersion:
             frame_passes_count = self.framePassesCount
             if frame_passes_count == 2:
-                imageVersion = "two_passes"
+                imageVersion = "two_passes" + (f"_{usdImageVersion}" if usdImageVersion is not None else "")
+            elif frame_passes_count == 1 and usdImageVersion is not None:
+                imageVersion = usdImageVersion
                 
         self.assertSnapshotClose(imageName, 
                                  self.IMAGEDIFF_FAIL_THRESHOLD,
@@ -101,10 +103,11 @@ class TestRefinement(mtohUtils.MayaHydraBaseTestCase):
         cmds.mayaHydra(updateRenderGlobals="mayaHydraRefinementLevel")
         self.verifySnapshot(imageName="basisCurves_refined_2.png", useDynamicVersion=True)
 
-        image_version = 'USD2508+' if self._usdVersion >= (0, 25, 8) else None
+        #Refinement level 3 was changed in USD 25.8+
+        _usdImageVersion = 'USD2508+' if self._usdVersion >= (0, 25, 8) else None
         cmds.setAttr("defaultRenderGlobals.mayaHydraRefinementLevel", 3)
         cmds.mayaHydra(updateRenderGlobals="mayaHydraRefinementLevel")
-        self.verifySnapshot(imageName="basisCurves_refined_3.png", useDynamicVersion=True)
+        self.verifySnapshot(imageName="basisCurves_refined_3.png", useDynamicVersion=True, usdImageVersion=_usdImageVersion)
 
         #restore the default
         cmds.setAttr("defaultRenderGlobals.mayaHydraRefinementLevel", 0)
