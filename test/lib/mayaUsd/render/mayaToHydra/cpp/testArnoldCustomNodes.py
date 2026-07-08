f0c778e69e0105321c38eacaf650f0e225d090f6
Ben Yoon <194553974+benyoon-adsk@users.noreply.github.com>
2026-07-08 15:40:31 -0400

    minor tweak

--- test/lib/mayaUsd/render/mayaToHydra/cpp/testArnoldCustomNodes.py
+++ test/lib/mayaUsd/render/mayaToHydra/cpp/testArnoldCustomNodes.py
@@ -38,9 +38,9 @@ class TestArnoldCustomNodes(mtohUtils.MayaHydraBaseTestCase):
     _requiredPlugins = ['mtoa']
     _setHdStormRenderer = False     # Need to use Arnold renderer for the tests
 
-    def tearDown(self):
-        """Test will hang if Arnold renderer is not reset before Maya exits."""
-        self.setViewport2Renderer()
+    # def tearDown(self):
+    #     """Test will hang if Arnold renderer is not reset before Maya exits."""
+    #     self.setViewport2Renderer()
 
     def _setArnoldRenderer(self):
         """Activate the Arnold Hydra renderer so mtoaSIP is in the scene index chain."""
